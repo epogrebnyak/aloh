6029de8a51063a9c376ffc81d1b3eb03142923f8
Evgeniy <e.pogrebnyak@gmail.com>
2020-11-29 22:04:56 +0300

    try package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+VERSION = '0.0.5'
+
+setup(name='aloh',
+      version=VERSION,
+      description='Order selection and production planning MILP with PuLP',
+      author='E Pogrebnyak',
+      author_email='e.pogrebnyak@gmail.com',
+      py_modules = ['aloh'],
+     )
