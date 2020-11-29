60193db9edb0f801c1d7bd70cdc8c09a14f05afc
Evgeniy <e.pogrebnyak@gmail.com>
2020-11-29 23:18:39 +0300

    moved examples

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
-VERSION = '0.0.5'
+VERSION = "0.0.5"
 
-setup(name='aloh',
-      version=VERSION,
-      description='Order selection and production planning MILP with PuLP',
-      author='E Pogrebnyak',
-      author_email='e.pogrebnyak@gmail.com',
-      py_modules = ['aloh'],
-     )
+setup(
+    name="aloh",
+    version=VERSION,
+    #description="Order selection and production planning MILP with PuLP",
+    #author="E Pogrebnyak",
+    #author_email="e.pogrebnyak@gmail.com",
+    py_modules=["aloh"],
+)
