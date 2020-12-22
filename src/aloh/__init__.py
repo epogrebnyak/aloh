dd601e7fe797c5079faf4071e9f858e09cadebc1
Evgeniy <e.pogrebnyak@gmail.com>
2020-12-22 18:07:16 +0300

    added package docstring

--- src/aloh/__init__.py
+++ src/aloh/__init__.py
@@ -1,3 +1,12 @@
+"""Select orders and plan discrete daily production for several products.
+
+Use *Product* class to formulate input information and *OptModel* to solve 
+linear programming model. 
+
+*DataframeViewer* shows modelling results as pandas dataframes.
+
+*generate_orders*, *Price*, *Volume* are used for order simulation.
+"""
 from .generate import Price, Volume, generate_orders
 from .interface import Product
 from .small import DataframeViewer, OptModel
