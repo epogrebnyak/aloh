79a0a0a39587090ee011686e3be5063ee7a96d66
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2021-02-11 09:39:39 +0300

    julia code for #2 example

--- src/julia/req.jl
+++ src/julia/req.jl
@@ -1,1 +1,12 @@
+# We need to: 
+# 1. ensure we write matrix elements in B at right positions
+# 2. can calculate fill requirement
+# 3. can calculate full requirement R based on B
+# 4. use R in the linoprog formularion properly (equality or inequality)
+
+# Support materials found in issue #2
+# https://github.com/epogrebnyak/aloh/issues/2
+
+# This is a direct rquirements matrix
+# We need 0.5 units of second product to make 1 unit of first product
 B = [0 0.5; 0 0]
